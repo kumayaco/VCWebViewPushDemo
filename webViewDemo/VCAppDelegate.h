0106409d7153c3db1f47212b32e10b979be1d89f
Vincent Chen <vince78718@gmail.com>
2014-06-19 19:42:55 +0800

    Initial Commit

---
+++ webViewDemo/VCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VCAppDelegate.h
+//  webViewDemo
+//
+//  Created by vincent on 2014/06/19.
+//  Copyright (c) 2014年 vc7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ webViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  webViewDemo
+//
+//  Created by vincent on 2014/06/19.
+//  Copyright (c) 2014年 vc7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VCAppDelegate class]));
+    }
+}
