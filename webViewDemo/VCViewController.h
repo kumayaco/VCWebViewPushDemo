c295e9bcb98c89950a0b91a943b95bbcdf712904
Vincent Chen <vince78718@gmail.com>
2014-06-19 19:55:48 +0800

    construct basic demo structure.

---
+++ webViewDemo/VCViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCViewController.h
+//  webViewDemo
+//
+//  Created by vincent on 2014/06/19.
+//  Copyright (c) 2014年 vc7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCViewController : UIViewController
+
+@end
